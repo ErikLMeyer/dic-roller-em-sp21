25ac578e1fd19a6cf1aa39514c4c5a1154cfb2a5
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-11 12:16:38 -0600

    Added a function

---
+++ diceroller.js
@@ -0,0 +1,5 @@
+// This function is not mine. I copied it from my professor's example because I feel it's too 
+// simple to do uniquely
+function RollSixSidedDice(){
+    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+}
