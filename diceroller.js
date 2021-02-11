16fff1fa97e40dd25ad718e3094e579d42de2e70
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-11 12:36:10 -0600

    Added boxes and buttons and a description

--- diceroller.js
+++ diceroller.js
@@ -1,5 +1,6 @@
 // This function is not mine. I copied it from my professor's example because I feel it's too 
-// simple to do uniquely
+// simple to do uniquely. Th only thing I did was edit the ID and duplicated the expression
 function RollSixSidedDice(){
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
 }
